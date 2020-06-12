70119986a53b7cfef1ff059b33073467b5ed8508
AmirulAiman <aiman_amirul@ymail.com>
2020-06-12 14:15:01 +0800

    change from company.create to register.create

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -5,7 +5,7 @@
         </div>
         <ul class="nav navbar-nav">
             <li class="{{ Route::currentRouteName() == 'system.home' ? "active" : "" }}"><a href="{{ Route('system.home') }}">Home</a></li>
-            <li class="{{ Route::currentRouteName() == 'company.create' ? "active" : "" }}"><a href="{{ Route('company.create') }}">Register Company</a></li>
+            <li class="{{ Route::currentRouteName() == 'register.create' ? "active" : "" }}"><a href="{{ Route('register.create',['type'=>'company']) }}">Register Company</a></li>
             <li class="{{ Route::currentRouteName() == 'admin.index' ? "active" : "" }}"><a href="{{ Route('admin.index') }}">Admin</a></li>
             <li class="{{ Route::currentRouteName() == 'user.index' ? "active" : "" }}"><a href="{{ Route('user.index') }}">User</a></li>
         </ul>
